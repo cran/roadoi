6c2a318b2e929fa85882851eae0e5df4a4fadab5
Najko Jahn <najko.jahn@gmail.com>
2021-07-29 11:20:02 +0000

    version 0.7.1

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -20,33 +20,3 @@ roadoi::oadoi_fetch(dois = c("10.1186/s12864-016-2566-9",
                     email = "najko.jahn@gmail.com", 
                     .progress = "text")
 
-## ---- message=FALSE-----------------------------------------------------------
-library(dplyr)
-library(rcrossref)
-# get a random sample of DOIs and metadata describing these works
-random_dois <- rcrossref::cr_r(filter = list(
-  issn = "2330-1643", type = "journal-article"
-  ), sample = 50)
-
-## -----------------------------------------------------------------------------
-oa_df <- roadoi::oadoi_fetch(random_dois, 
-                             email = "najko.jahn@gmail.com")
-
-## -----------------------------------------------------------------------------
-oa_df
-
-## -----------------------------------------------------------------------------
-oa_df %>%
-  group_by(is_oa) %>%
-  summarise(Articles = n()) %>%
-  mutate(Proportion = Articles / sum(Articles)) %>%
-  arrange(desc(Articles))
-
-## -----------------------------------------------------------------------------
-oa_df %>%
-  filter(is_oa == TRUE) %>%
-  tidyr::unnest(oa_locations) %>% 
-  group_by(oa_status, evidence, is_best) %>%
-  summarise(Articles = n()) %>%
-  arrange(desc(Articles))
-
